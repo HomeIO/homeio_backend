888964ff3277521c8d816d640f2ea0f588c5f558
Aleksander Kwiatkowski <bobikx@poczta.fm>
2015-04-29 16:11:29 +0200

    fix

--- src/backend/meas_buffer.cpp
+++ src/backend/meas_buffer.cpp
@@ -10,8 +10,10 @@ MeasBuffer::MeasBuffer(unsigned long int _maxSize = 1000000) {
 }
 
 unsigned long int MeasBuffer::add(unsigned int raw) {
-  // must be here because in other case buffer[0] = 0
-  buffer[offset] = raw;
+  // must be here because in other case first measurement is equal 0
+  if (count == 0) {
+    buffer[offset] = raw;
+  }
   
   offset++;
   if (offset >= maxSize) {
@@ -26,7 +28,7 @@ unsigned long int MeasBuffer::add(unsigned int raw) {
   }
   lastTime = mTime();
   
-  //buffer[offset] = raw;
+  buffer[offset] = raw;
   
   return offset;
 }
