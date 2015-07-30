af8a74e4af5b6c1d4065bf75ddf32d776e1e0ce2
Aleksander Kwiatkowski <bobikx@poczta.fm>
2015-07-30 15:31:06 +0200

    clean run script now use makefile

--- start.sh
+++ start.sh
@@ -1,4 +1,1 @@
-rm ./bin/homeio_main
-g++ -std=gnu++11 -g -oterm -I /usr/include -lpthread src/mains/main.cpp -o bin/homeio_main
-sudo bin/homeio_main
-#sudo gdb bin/homeio_main
+make run SITE=wind
--- start_arduino.sh
+++
@@ -1,4 +0,0 @@
-rm ./bin/homeio_main_arduino
-g++ -std=gnu++11 -g -oterm -I /usr/include -lpthread src/mains/main_arduino.cpp -o bin/homeio_main_arduino
-sudo bin/homeio_main_arduino
-#sudo gdb bin/homeio_main_arduino
