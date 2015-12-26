bb558670082512424b777e1f5f5d7aec39813244
Aleksander Kwiatkowski <bobikx@poczta.fm>
2015-12-26 09:36:16 +0100

    wind turbine overseer changes

--- src/mains/main_wind.cpp
+++ src/mains/main_wind.cpp
@@ -12,7 +12,7 @@ int main()
 {
   h = new HomeIO();
   signal(SIGINT, handleSignal);
-  
+
   // IoProxy connects to IoServer.
   // IoServer acts as bridge between uC via UART/RS/USB/hardware using TCP protocol.
   // It gets command, send to hardware, get hardware response and trasmit it via TCP.
@@ -29,18 +29,18 @@ int main()
 
   // Now please add type of measurements which you want backend to fetch and
   // store in RAM buffer.
-  
+
   MeasType *m;
-  
+
   // note: test meas is not needed
   //m = new MeasType();
   // this must be unique and will be used by frontend and overseers
   //m->name = "test_t";
-  // ascii command to get measurement 
+  // ascii command to get measurement
   //m->command = 't';
-  // number of bytes of response 
+  // number of bytes of response
   //m->responseSize = 2;
-  // value = (raw + coefficientOffset)* coefficientLinear 
+  // value = (raw + coefficientOffset)* coefficientLinear
   //m->coefficientLinear = 1.0;
   //m->coefficientOffset = 0;
   // do not store measurements if they are 'too short', in miliseconds
@@ -49,10 +49,10 @@ int main()
   //m->maxTimeDiffToStore = 3600000;
   // store measurement if value changed enough
   //m->valueDiffToStore = 1.0;
-  
+
   // add this measuremt type to measurement which will be fetched by this server
   //h->measTypeArray->add(m);
-  
+
   //m = new MeasType();
   //m->name = "test_s";
   //m->command = 's';
@@ -73,7 +73,7 @@ int main()
   m->priority = 1;
   h->measTypeArray->add(m);
 
-  
+
   m = new MeasType();
   m->name = "i_gen_batt";
   m->unit = "A";
@@ -83,7 +83,7 @@ int main()
   m->coefficientOffset = -512;
   m->priority = 1;
   h->measTypeArray->add(m);
-  
+
   m = new MeasType();
   m->name = "i_gen_resist";
   m->unit = "A";
@@ -92,7 +92,7 @@ int main()
   m->coefficientLinear = 0.191;
   m->coefficientOffset = -512;
   h->measTypeArray->add(m);
-  
+
   m = new MeasType();
   m->name = "i_inverters";
   m->unit = "A";
@@ -101,16 +101,16 @@ int main()
   m->coefficientLinear = 0.191;
   m->coefficientOffset = -512;
   h->measTypeArray->add(m);
-  
+
   m = new MeasType();
   m->name = "imp_per_min";
   m->unit = "imp/min";
   m->command = 'l';
   m->responseSize = 2;
   m->coefficientLinear = 60.0;
-  m->coefficientOffset = 0;  
+  m->coefficientOffset = 0;
   h->measTypeArray->add(m);
-  
+
   m = new MeasType();
   m->name = "coil_1_u";
   m->unit = "V";
@@ -119,7 +119,7 @@ int main()
   m->coefficientLinear = 0.0777126099706744868;
   m->coefficientOffset = 0;
   h->measTypeArray->add(m);
-  
+
   m = new MeasType();
   m->name = "coil_2_u";
   m->unit = "V";
@@ -128,7 +128,7 @@ int main()
   m->coefficientLinear = 0.0777126099706744868;
   m->coefficientOffset = 0;
   h->measTypeArray->add(m);
-  
+
   m = new MeasType();
   m->name = "coil_3_u";
   m->unit = "V";
@@ -137,21 +137,21 @@ int main()
   m->coefficientLinear = 0.0777126099706744868;
   m->coefficientOffset = 0;
   h->measTypeArray->add(m);
-  
+
   m = new MeasType();
   m->name = "res_pwm";
   m->unit = "pwm";
   m->command = 'p';
   m->responseSize = 2;
   h->measTypeArray->add(m);
-  
+
   m = new MeasType();
   m->name = "res_pwm_avg";
   m->unit = "pwm";
   m->command = 'q';
   m->responseSize = 2;
   h->measTypeArray->add(m);
-  
+
   m = new MeasType();
   m->name = "outputs";
   m->unit = "bit array";
@@ -160,27 +160,27 @@ int main()
   h->measTypeArray->add(m);
 
   // end measurements here
-  
+
   IoServer *io = new IoServer();
   // port device connect to
   // example: "/dev/ttyUSB0", "/dev/ttyS0"
   io->port = "/dev/ttyUSB0";
   h->ioServer = io;
-  
+
   // meas fetcher intervals
   h->measFetcher->betweenMeasInterval = 10000;
   h->measFetcher->cycleInterval = 50000;
-  
+
   h->fileStorage->cycleInterval = 24*3600*1000;
-  
+
   // actions
   ActionType *a;
   a = new ActionType();
   // this must be unique and will be used by frontend and overseers
   a->name = "start_total_brake";
-  // ascii command to get measurement 
+  // ascii command to get measurement
   a->command = 'w';
-  // number of bytes of response 
+  // number of bytes of response
   a->responseSize = 1;
   // "okay" response value
   a->responseOkay = 21;
@@ -190,15 +190,15 @@ int main()
   a = new ActionType();
   // this must be unique and will be used by frontend and overseers
   a->name = "turn_off_brake";
-  // ascii command to get measurement 
+  // ascii command to get measurement
   a->command = 'W';
-  // number of bytes of response 
+  // number of bytes of response
   a->responseSize = 1;
   // "okay" response value
   a->responseOkay = 20;
   // add this measuremt type to measurement which will be fetched by this server
   h->actionTypeArray->add(a);
-  
+
 
   a = new ActionType();
   a->name = "inverter_A_off"; // "output_2_off";
@@ -206,33 +206,33 @@ int main()
   a->responseSize = 1;
   a->responseOkay = 1;
   h->actionTypeArray->add(a);
-  
+
   a = new ActionType();
   a->name = "inverter_B_off"; // "output_3_off";
   a->command = 'c';
   a->responseSize = 1;
   a->responseOkay = 2;
   h->actionTypeArray->add(a);
-  
+
   a = new ActionType();
   a->name = "inverter_A_on"; // "output_2_on";
   a->command = 'B';
   a->responseSize = 1;
   a->responseOkay = 11;
   h->actionTypeArray->add(a);
-  
+
   a = new ActionType();
   a->name = "inverter_B_on"; // "output_3_on";
   a->command = 'C';
   a->responseSize = 1;
   a->responseOkay = 12;
   h->actionTypeArray->add(a);
-  
 
-  
+
+
   // overseers intervals
   h->overseerArray->cycleInterval = 2000000;
-  
+
   // overseers
   Overseer *o;
   o = new Overseer();
@@ -285,7 +285,7 @@ int main()
   o->thresholdValue = 2.0;
   o->isMax = false;
   o->windowSize = 50;
-  h->overseerArray->add(o);  
+  h->overseerArray->add(o);
 
   o = new Overseer();
   o->name = "turn_off_inverter_B_on_low_coil";
@@ -294,13 +294,13 @@ int main()
   o->thresholdValue = 2.0;
   o->isMax = false;
   o->windowSize = 50;
-  h->overseerArray->add(o);  
+  h->overseerArray->add(o);
 
   o = new Overseer();
   o->name = "turn_on_inverter_B_on_high_battery";
   o->actionName = "inverter_B_on";
   o->measName = "batt_u";
-  o->thresholdValue = 43.0;
+  o->thresholdValue = 30.0;
   o->isMax = true;
   o->windowSize = 1000;
   h->overseerArray->add(o);
@@ -309,7 +309,7 @@ int main()
   o->name = "turn_off_inverter_B_on_low_battery";
   o->actionName = "inverter_B_off";
   o->measName = "batt_u";
-  o->thresholdValue = 33.0;
+  o->thresholdValue = 23.0;
   o->isMax = false;
   o->windowSize = 100;
   h->overseerArray->add(o);
@@ -318,7 +318,7 @@ int main()
   o->name = "turn_off_inverter_A_on_low_battery";
   o->actionName = "inverter_A_off";
   o->measName = "batt_u";
-  o->thresholdValue = 27.0;
+  o->thresholdValue = 23.0;
   o->isMax = false;
   o->windowSize = 100;
   h->overseerArray->add(o);
@@ -328,7 +328,7 @@ int main()
   h->frontendSettings->intervalCurrent = 1000;
   h->frontendSettings->intervalHistory = 3600*1000;
   h->frontendSettings->currentCoeff = 0.4;
-  
+
   // WARNING
   // this announce measurements to global server used for statistics and uptime
   h->spy->enabled = true;
@@ -336,18 +336,18 @@ int main()
   h->spy->hiveHost = "http://hive.homeio.org.pl";
   h->spy->siteName = "lakie_dev";
   h->spy->enabled = true;
-  
+
   // buffer storage
   h->measBufferBackupStorage->cycleInterval = (long) 10*60*1000*1000; // 10 minutes
   h->measBufferBackupStorage->thresholdTimeRange = (long) 20*60*1000*1000; // 20 minutes
   h->measBufferBackupStorage->usDelay = 60*1000*1000; // 1 minute
-  
-  // ram buffer resize 
+
+  // ram buffer resize
   h->measFetcher->maxBufferSize = 4000000;
-  
-  // Now you can run everything of parts of backend. 
+
+  // Now you can run everything of parts of backend.
   // TODO: way to disable IoServer
   h->start();
-  
+
   return 0;
 }
